68680b8f3f477d9f846f8b6a9244dbad2acb4623
Chamara-Wijepala <mr.worf777@gmail.com>
2022-09-09 17:56:07 +0530

    Move LogIn and LogOut into their own components

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,51 @@ import { useAuthState } from "react-firebase-hooks/auth";
 
 import { auth, provider } from "./firebase-config";
 
+import googleLogo from "./assets/google-logo.png";
+
 function App() {
   const [user] = useAuthState(auth);
 
   return (
     <>
       <header>
-        {user ? (
-          <div>
-            <img src={user.photoURL!} alt="profile picture" />
-            <button
-              onClick={() => {
-                signOut(auth);
-              }}
-            >
-              Sign out
-            </button>
-          </div>
-        ) : (
-          <button
-            onClick={() => {
-              signInWithPopup(auth, provider);
-            }}
-          >
-            Sign in with Google
-          </button>
-        )}
+        <div className="container header-container">
+          {user ? <LogOut /> : <LogIn />}
+        </div>
       </header>
     </>
+  );
+}
+
+function LogOut() {
+  const photo = auth.currentUser?.photoURL;
+
+  return (
+    <>
+      <img src={photo!} alt="profile picture" />
+      <button
+        className="btn"
+        onClick={() => {
+          signOut(auth);
+        }}
+      >
+        Sign out
+      </button>
+    </>
+  );
+}
+
+function LogIn() {
+  return (
+    <button
+      className="btn login-with-google-btn"
+      onClick={() => {
+        signInWithPopup(auth, provider);
+      }}
+    >
+      <img src={googleLogo} alt="google logo" />
+      Sign in with Google
+    </button>
   );
 }
 
